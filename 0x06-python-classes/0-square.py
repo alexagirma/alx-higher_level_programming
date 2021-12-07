261f65fb3b69be16edb94d2662e392e4e35ea48e
Alexagirma <alexagirma255@gmail.com>
2021-12-07 04:32:16 -0800

    update classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Square Class
-This class don't do anything for the moment
+"""
+Defines an empty class Square
 """
 
 
 class Square:
+    """ empty class """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""Square Class
-A Square Class
+"""
+Defines an empty class Square with size attribute
 """
 
 
 class Square:
-
+    """ Defines Square class with private size attribute """
     def __init__(self, size):
-        """__init__
-        The __init__ method initializes the size value
-        of the square.
-        Attributes:
-            size (int): The size of the square.
-        """
         self.__size = size
