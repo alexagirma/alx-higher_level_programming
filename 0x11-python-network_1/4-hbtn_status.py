0c1d8e0bc861b35b41c064c05d179ef3c8c054a6
Alexagirma <alexagirma255@gmail.com>
2022-03-18 05:19:03 -0700

    updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+""" Fetches https://intranet.hbtn.io/status"""
 import requests
 
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+if __name__ == '__main__':
+    s = requests.Session()
+
+    response = s.get('https://intranet.hbtn.io/status')
+    body = response.text
+
+    print('Body response:')
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
