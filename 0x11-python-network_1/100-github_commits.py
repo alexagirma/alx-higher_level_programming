40e39ee2f6f807bc5ff3ff72c2320efd33e34e86
Alexagirma <alexagirma255@gmail.com>
2022-03-18 05:22:13 -0700

    updated

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/python3
+""" Fetches https://intranet.hbtn.io/status"""
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+
+if __name__ == '__main__':
+    repo, user = argv[1:]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(user, repo)
+
+    s = requests.Session()
+
+    response = s.get(url)
+    commits = response.json()[:10]
+    for commit in commits:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
